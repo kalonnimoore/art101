fe823f06e85f4e48871494cb86c2ccf17686f50f
Diamond-M <dikmoore@ucsc.edu>
2022-10-24 13:42:36 -0700

    lab5 and more

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/* Author: Diamond Moore <dikmoore@ucsc.edu>
+* Created: 18 October 2022
+* License: Public Domain
+*/
+
+// Define Variables
+make = "Diamond";
+model = "Feet";
+color = "Shiny";
+year = 2002;
+
+// calculate
+age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
